daeeb5ba533600a1ada9d2db8c978dcce645c3a4
TeeEye <wangchenghao@mokahr.com>
2019-06-12 10:31:09 +0800

    bug fix

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -2,45 +2,17 @@ import sys
 sys.path.append("..")
 
 import pickle
-import pandas as pd
 import numpy as np
 from utils.macros import *
 from utils.embedding import Embedding
 
 
-if __name__ == '__main__':
-    # 目前只使用第一个训练数据
-    current_train_file = 0
-    raw_data_path = DATA_PATH % current_train_file
-    train_data_path = TRAIN_PATH % current_train_file
-
-    # 先将所有数据合并
-    current_count = 0
-    with open(raw_data_path, 'rb') as f:
-        app_arr = []
-        while current_count < TRAIN_SIZE:
-            try:
-                app = pickle.load(f)
-                app = app[['candidate_summary', 'job_description', 'job_class_1']]
-                app = app[app['candidate_summary'].str.len() > 1]
-                app = app[app['job_description'].str.len() > 1]
-                app.dropna(inplace=True)
-                app_arr.append(app)
-                current_count += len(app)
-                print('Data loaded ', len(app))
-            except EOFError:
-                break
-        print('All data loaded!')
-    print('Start data cleaning...')
-    app = pd.concat(app_arr, ignore_index=True)
-    app = app.head(TRAIN_SIZE)
-    app['label'] = 1
+def shuffle_data(app):
+    # 生成负样本
+    print('Shuffling data...')
     total_len = len(app)
-    app.reset_index(drop=True, inplace=True)
-    print('Data cleaning finished!')
-
-    for a in range(0, total_len-1, 2):
-        b = (a + total_len//2) % total_len
+    for a in range(0, total_len - 1, 2):
+        b = (a + total_len // 2) % total_len
         row_a = app.iloc[a]
         row_b = app.iloc[b]
         cls_a = row_a['job_class_1']
@@ -57,26 +29,60 @@ if __name__ == '__main__':
         app.iloc[b, 1] = tmp_description
         app.iloc[b, 2] = cls_a
         app.iloc[b, 3] = 0
-        if a % 1000 == 0:
-            sys.stdout.write('\rProcessing %d / %d' % (a, total_len))
-            sys.stdout.flush()
     print('\nDone!')
-    print('Converting words into vecs')
+
+
+def text2vec(app):
+    total_len = len(app)
+    print('Converting words into vec')
     summary = []
     description = []
-    embedding = Embedding()
     for idx, row in app.iterrows():
         summary.append(embedding.sentence2vec(row[0]))
         description.append(embedding.sentence2vec(row[1]))
         if idx % 1000 == 0:
             sys.stdout.write('\rProcessing %d / %d' % (idx, total_len))
             sys.stdout.flush()
-    del embedding
-    print('memory released!')
+    del app['candidate_summary']
+    del app['job_description']
     app['candidate_summary'] = np.asarray(summary)
     app['job_description'] = np.asarray(description)
     print('\nDone!')
-    print('Saving final data...')
-    with open(train_data_path, 'wb') as f:
-        pickle.dump(app, f)
-    print('Done!')
+
+
+def run():
+    # 目前只使用第一个训练数据
+    current_train_file = 0
+    raw_data_path = DATA_PATH % current_train_file
+    output_data_path = TRAIN_PATH % current_train_file
+
+    output_file = open(output_data_path, 'wb')
+
+    with open(raw_data_path, 'rb') as f:
+        app_arr = []
+        while True:
+            try:
+                app = pickle.load(f)
+                print('Data loaded ', len(app))
+                app = app[['candidate_summary', 'job_description', 'job_class_1']]
+                app = app[app['candidate_summary'].str.len() > 1]
+                app = app[app['job_description'].str.len() > 1]
+                app.dropna(inplace=True)
+                app['label'] = 1
+                app.reset_index(drop=True, inplace=True)
+                for offset in range(0, len(app), TRAIN_SIZE):
+                    app_batch = app.iloc[offset:offset+TRAIN_SIZE]
+                    shuffle_data(app_batch)
+                    text2vec(app_batch)
+                    pickle.dump(app_batch, output_file)
+                del app
+            except EOFError:
+                break
+
+    output_file.close()
+    print('All done!')
+
+
+if __name__ == '__main__':
+    embedding = Embedding()
+    run()
