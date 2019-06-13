ef9a7e2a17f8b656a5438e69a63e9ac77009672b
TeeEye <wangchenghao@mokahr.com>
2019-06-13 11:44:12 +0800

    bug fix

--- utils/embedding.py
+++ utils/embedding.py
@@ -1,9 +1,14 @@
 from utils.macros import *
 import pickle
 import numpy as np
+import jieba
 
 
 class Embedding:
+    """
+    接口:
+        sentence2vec: 将中文句子转化为向量 ndarray
+    """
     def __init__(self):
         print('Building tencent embedding...')
         self.word2vec = {}
@@ -21,8 +26,13 @@ class Embedding:
         print('Embedding initiated!')
 
     def sentence2vec(self, sentence):
+        """
+        将中文句子转化为词向量的 ndarray
+        :param sentence: 中文句子
+        :return: 词向量 ndarray
+        """
         result = []
-        for word in sentence:
+        for word in jieba.cut(sentence):
             if len(result) >= PAD_SIZE:
                 break
             if word not in self.word2vec:
